4e6761084c453ec08e0b2befc58c42c4b888916f
MiserableLife <kdw6485@gmail.com>
2019-05-09 23:38:20 +0000

    [feat] common fixed
    common fixed and cmakelist at top fixed

--- src/common/common.h
+++ src/common/common.h
@@ -1,8 +1,14 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-#define 	SERVER_PORT 9303
-#define 	CLIENT_READ_PORT 9210
+#define 	SERVER_PORT 			9303
+#define 	COMMAND_PORT 			9210
+#define 	CLIENT_READ_PORT 	9210
+#define 	IMAGE_SIZE 				921600
+#define 	IMAGE_WIDTH 			640
+#define 	IMAGE_HEIGHT 			480
+#define 	IMAGE_STEP 				1920
+
 enum
 {
 	PERIOD=1
