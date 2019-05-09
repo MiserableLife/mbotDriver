e6cef450125c06733a53f669742a27204f04dad8
MiserableLife <kdw6485@gmail.com>
2019-05-09 23:04:38 +0000

    [feat] server program
    server program uses tcp server and udp server

---
+++ src/server/main.cpp
@@ -0,0 +1,36 @@
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2016 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include "udp_server.h"
+#include "tcp_server.h"
+
+
+
+
+int main()
+{
+  try
+  {
+    boost::asio::io_service io_service;
+    TcpServer server(io_service, SERVER_PORT);
+		UdpServer commserver(io_service );
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
