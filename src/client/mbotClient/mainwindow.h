a3d934a1e337db26f0473a99bd9dfc373aaaf3dd
MiserableLife <kdw6485@gmail.com>
2019-05-21 09:22:35 +0000

    [feat] client for key command
    client send udp for key command
    
    right, left, up, down

--- src/client/mbotClient/mainwindow.h
+++ src/client/mbotClient/mainwindow.h
@@ -24,8 +24,24 @@ public:
 
     void keyPressEvent(QKeyEvent *ev)
     {
-           std::cout<< ev->text().toUtf8().data()<<" pressed!"<<std::endl;
-           client->send_cmd(ev->key(), cmd_ep);
+
+           int key = ev->key();
+           switch(key)
+           {
+           case Qt::Key_Left:
+           case Qt::Key_Up:
+           case Qt::Key_Down:
+           case Qt::Key_Right:
+               std::cout<<key<<" sent!"<<std::endl;
+               break;
+           default:
+               return;
+           }/*
+           Qt::Key_Left	0x01000012
+           Qt::Key_Up	0x01000013
+           Qt::Key_Right	0x01000014
+           Qt::Key_Down	0x01000015	 */
+           client->send_cmd(key, cmd_ep);
 
     }
 
